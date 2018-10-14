23b5daac7d4849b2aa9ef37296af7a1bcf25af35
arolave <andres@thinkingbytes.net>
2018-10-14 15:06:16 +0300

    remote test

--- test/index.test.js
+++ test/index.test.js
@@ -5,7 +5,7 @@ describe('a test', () => {
     expect(1 + 2).toBe(3)
   })
 
-  it('should work', () => {
-    expect(2 + 2).toBe(4)
-  })
+  // it('should work', () => {
+  //   expect(2 + 2).toBe(4)
+  // })
 })
