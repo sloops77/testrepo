1eebe161812c1f66e6cbde75dc24c031cad6cd41
Thinking Byter <sloops77@users.noreply.github.com>
2018-10-25 14:28:39 +0300

    Test1 54 (#49)
    * a commit
    
    * add all sorts of goodies
    
    * and more
    
    * add coveralls
    
    * fix ci
    
    * fixes for testing
    
    * ci fixes
    
    * ci fixes
    
    * ci fixes
    
    * fix test
    
    * fail test
    
    * fix
    
    * test fail
    
    * fix
    
    * test fix
    
    * test
    
    * change
    
    * fix
    
    * test
    
    * fix
    
    * test
    
    * test
    
    * fail
    
    * fix
    
    * fail
    
    * fix
    
    * fail
    
    * fix
    
    * fail
    
    * fix
    
    * fix
    
    * test2
    
    * add test
    
    * remote test
    
    * added tests
    
    * new test
    
    * removed test
    
    * fail test
    
    * fix
    
    * more tests
    
    * reas
    
    * remove test
    
    * done
    
    * lint fixed
    
    * test failure buton
    
    * test
    
    * test
    
    * fixed

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+require('../src/index')
+
+describe('a test', () => {
+  it('should work', () => {
+    expect(1 + 2).toBe(3)
+  })
+
+  it('should work', () => {
+    expect(2 + 2).toBe(4)
+  })
+})
